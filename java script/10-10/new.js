08d67d256879d0193727a1e1594cbf7b93bb76d4
deepak <depusharma94742@gmail.com>
2025-10-10 12:54:00 +0530

    add files

---
+++ java script/10-10/new.js
@@ -0,0 +1,81 @@
+// const random = Math.random()*100
+// console.log(random)
+
+
+// let a= 25
+// let ceil=Math.ceil(a)
+// let floor=Math.floor(a)
+// let round=Math.round(a)
+// let abs=Math.abs(a)
+// let trunc=Math.trunc(a)
+// let sqrt=Math.sqrt(a)
+
+
+
+
+
+
+// let b = [5, 20, 30, 92, 32, 72];
+// let max = Math.max(...b);
+// console.log(max); // Output: 92
+
+let max = Math.max(5, 20, 30, 92, 32, 72);
+console.log(max); // Output: 92
+
+
+
+
+
+// console.log(ceil)
+// console.log(floor)
+// console.log(round)
+// console.log(abs)
+// console.log(trunc)
+// console.log(sqrt)
+
+
+// console.log(Math.abs(-7));
+// console.log(Math.abs(7));  
+
+
+
+// let person = {
+//     name : "deepak",
+//     age : 20,
+//     gender : "male"
+
+// }
+
+
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.gender)
+
+
+// console.log(person["name"])
+// console.log(person["age"])
+// console.log(person["gender"])
+
+
+
+// const{
+//     name,age,gender
+// }=person
+// console.log(name)
+// console.log(age)
+
+
+// person.name = "deepu"
+// console.log(person)
+// person.class ="bca"
+// console.log(person)
+
+
+// const a =
+// {
+//     name : "deepak",
+//     rollNo : 12,
+//     working : true,
+// }
+
